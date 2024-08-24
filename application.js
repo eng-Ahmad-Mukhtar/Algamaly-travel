b7ac88a2b84f017285316593aceca0373d96c776
Ahmed Mukhtar Muhammed <aamedmukhtar@gmail.com>
2024-08-24 04:57:08 +0300

    Update application for Vercel deployment

--- application.js
+++ application.js
@@ -107,7 +107,7 @@
 // ==============
 const express = require('express');
 const Application = express();
-
+const path = require('path');
 // require('dotenv').config();
 const port = process.env.PORT || 1000;
 
