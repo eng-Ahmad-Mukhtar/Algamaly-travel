a82c45ac5dc1abbd3057b82b74bbbe17cdba069d
Ahmed Mukhtar Muhammed <aamedmukhtar@gmail.com>
2024-08-24 04:21:22 +0300

    Update application for Vercel deployment

--- application.js
+++ application.js
@@ -170,8 +170,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-Application.set('view engine', 'ejs');
-Application.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
