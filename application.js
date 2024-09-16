c29ba594eb6c4d40060e1f25f7aec7d61173b6c5
Ahmed Mukhtar Muhammed <aamedmukhtar@gmail.com>
2024-09-16 03:35:28 +0300

    Update application for Vercel deployment

--- application.js
+++ application.js
@@ -70,36 +70,9 @@ const loguser = require('./Router/users');
 Application.use(loguser);
 
 
-const multer = require('multer');
 
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  }
-});
-const upload = multer({ storage: storage });
-
-Application.post('/upload-image', upload.single('image'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: 'No file uploaded' });
-  }
-
-
-  const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
-
-
-
-  res.json({ imageUrl });
-});
-
-
-Application.use('/uploads', express.static('uploads'));
 
 
 Application.listen(port, () => {
