8b8c6dc5e12dce3118c5569dd7cac52b3078d36b
Ahmed Mukhtar Muhammed <aamedmukhtar@gmail.com>
2024-09-23 20:56:21 +0300

    Update application for Vercel deployment

--- application.js
+++ application.js
@@ -1,55 +1,32 @@
-
-
-
-
-
 const express = require('express');
-
-
 const Application = express();
-
-
 const path = require('path');
 require('dotenv').config();
 const port = process.env.PORT || 1000;
-
 Application.set('view engine', 'ejs');
-
 Application.use(express.static('uploads'));
 Application.use(express.static(path.join(__dirname, 'public')));
 Application.set('views', path.join(__dirname, 'views'));
-
 Application.use(express.urlencoded({ extended: false }));
 Application.use(express.json());
-
-
 const session = require('express-session');
 const { SESSION_SECRET } = process.env;
-
-
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
-
 const MongoDBStore = require('connect-mongodb-session')(session);
-
-
 const store = new MongoDBStore({
   uri: process.env.DATABASE_CONNECTION,
   collection: 'sessions'
 });
-
 store.on('error', function (error) {
   console.error('MongoDB Session Store Error:', error);
 });
-
-
 Application.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -61,26 +38,13 @@ Application.use(
     }
   })
 );
-
-
 const home = require('./Router/home_route');
 Application.use(home);
-
 const loguser = require('./Router/users');
 Application.use(loguser);
-
-
-
-
-
-
-
 Application.listen(port, () => {
   console.log(`Application running on http://localhost:${port}`);
 })
-
-
-
 Application.use((req, res) => {
   res.status(404).send('لا يوجد صفحه ');
 });
